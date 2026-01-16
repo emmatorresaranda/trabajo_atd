b6e831dbb5368b4ff715d02dd5a406c204f4f184
emmatorresaranda <emmatorresaranda@gmail.com>
2026-01-16 16:16:55 +0100

    Update SELENIUMfinal.py

--- src/SELENIUMfinal.py
+++ src/SELENIUMfinal.py
@@ -14,6 +14,7 @@ with open("arwu_bs4.csv", newline="", encoding="utf-8") as f:
     reader = csv.reader(f,delimiter=';')
     next(reader)  # saltar cabecera
     for row in reader:
+        
         country=row[1]
         if country not in countries:
             countries.append(country)
@@ -23,12 +24,12 @@ url = "https://ourworldindata.org/grapher/share-of-the-population-with-completed
 driver = webdriver.Chrome()
 driver.get(url)
 
-# Esperar a que cargue la tabla
-wait = WebDriverWait(driver, 20)  # hasta 20 segundos
-
+# Esperar a que cargue la tabla hasta 20 segundos
+wait = WebDriverWait(driver, 20)  
 table = wait.until(EC.presence_of_element_located((By.TAG_NAME, "table")))
 rows = table.find_elements(By.TAG_NAME, "tr")
 
+# Creamos un diccionario con los datos de la tabla
 data_dict = {}
 for row in rows[1:]:
     cols = row.find_elements(By.TAG_NAME, "td")
@@ -37,16 +38,13 @@ for row in rows[1:]:
     name = cols[0].text.strip()
     value = cols[2].text.strip()
     data_dict[name.lower()] = value
-
+    
+# Buscamos los índices de los países del ranking
 tertiary_education = {}
 for country in countries:
-    # buscar en data_dict por minúsculas, si no existe poner "No encontrado"
     tertiary_education[country] = data_dict.get(country.lower(), "No encontrado")
 
-# Mostrar resultado como diccionario
-print(tertiary_education)
-
-# Guardar CSV con formato: Pais;Tasa_educacion_terciaria
+# Guardar la información obtenida en un CSV
 with open("tertiary_education_simple.csv", "w", newline="", encoding="utf-8") as f:
     w = csv.writer(f, delimiter=';')
     w.writerow(["Pais", "Tasa_educacion_terciaria"])
@@ -54,5 +52,4 @@ with open("tertiary_education_simple.csv", "w", newline="", encoding="utf-8") as
         w.writerow([pais, valor])
 
 driver.quit()
-
 print("CSV generado correctamente: tertiary_education_simple.csv")
